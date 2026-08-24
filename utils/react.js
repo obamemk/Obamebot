995d7807045fce2751458236a2dfa3b81e148c2a
obamemk <paulprincemr@gmail.com>
2026-08-24 18:00:35 +0100

    Create utils/react.js

---
+++ utils/react.js
@@ -0,0 +1,34 @@
+export default async function react(client, message) {
+
+    const sleep = ms =>
+        new Promise(resolve => setTimeout(resolve, ms))
+
+    const remoteJid = message?.key?.remoteJid
+
+    if (!remoteJid || !message?.key) return
+
+    await client.sendMessage(remoteJid, {
+        react: {
+            text: '🎯',
+            key: message.key
+        }
+    })
+
+    await sleep(1000)
+
+    await client.sendMessage(remoteJid, {
+        react: {
+            text: '⚡',
+            key: message.key
+        }
+    })
+
+    await sleep(1000)
+
+    await client.sendMessage(remoteJid, {
+        react: {
+            text: '',
+            key: message.key
+        }
+    })
+}
